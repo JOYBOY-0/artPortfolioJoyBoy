de0d9f5132824191af197762d1d8eddd2543bbd0
Your Name <you@example.com>
2021-07-25 00:14:13 -0300

    header update

--- src/routes/web.js
+++ src/routes/web.js
@@ -6,7 +6,9 @@ const path = require('path')
 // Website Routes
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "../views", "index.html")));
-app.get("/character", (req, res) => res.sendFile(path.resolve(__dirname, "../views", "character.html")));
+app.get("/portfolio", (req, res) => res.sendFile(path.resolve(__dirname, "../views", "portfolio.html")));
 app.get("/contact", (req, res) => res.sendFile(path.resolve(__dirname, "../views", "contact.html")));
+app.get("/aboutMe", (req, res) => res.sendFile(path.resolve(__dirname, "../views", "aboutMe.html")));
+
 
 module.exports = app
